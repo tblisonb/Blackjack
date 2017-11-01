e9af5eb9d5c0a3522e668281274f6a03af17bf95
Tanner Lisonbee <lisonbeetanner@gmail.com>
2017-11-01 14:53:00 -0700

    Added Card functionality

--- Blackjack/src/blackjack/Card.java
+++ Blackjack/src/blackjack/Card.java
@@ -1,14 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package blackjack;
 
 /**
- *
  * @author Tanner Lisonbee
  */
-public class Card {
+public class Card 
+{
+    public enum Suit { HEARTS, SPADES, CLUBS, DIAMONDS }
+    public enum Number 
+    { 
+        ACE(1), TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), 
+        NINE(9), TEN(10), JACK(10), QUEEN(10), KING(10);
+        
+        private final int value;
+        Number (int value) 
+        {
+            this.value = value;
+        }
+    }
     
+    private Suit suit;
+    private Number number;
+    
+    public Card(Suit suit, Number number)
+    {
+        this.suit = suit;
+        this.number = number;
+    }
+
+    public Suit getSuit() 
+    {
+        return suit;
+    }
+
+    public Number getNumber() 
+    {
+        return number;
+    }
 }
