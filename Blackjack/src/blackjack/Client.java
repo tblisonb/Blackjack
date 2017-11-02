bc986844ff6d64098efe65effc3b005b48d5ae8f
Tanner Lisonbee <Tanner Lisonbee@Actarius-MkII>
2017-11-02 14:41:57 -0700

    Added rudimentary GUI design and graphics (will be updated later)

--- Blackjack/src/blackjack/Client.java
+++ Blackjack/src/blackjack/Client.java
@@ -2,6 +2,8 @@ package blackjack;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -13,8 +15,26 @@ public class Client extends Application
     @Override
     public void start(Stage primaryStage)
     {
+        BorderPane pane = new BorderPane();
+        ImageView image = new ImageView("images/Blackjack.jpg");
+        
+        image.fitWidthProperty().bind(primaryStage.widthProperty());
+        image.fitHeightProperty().bind(primaryStage.heightProperty());
+        pane.setCenter(image);
+        pane.setId("pane");
+        
+        //StackPane root = new StackPane();
+        //root.setId("pane");
+        Scene scene = new Scene(pane, 1280, 720);
+        scene.getStylesheets().add("images/style.css");
         primaryStage.setTitle("Blackjack");
-        primaryStage.setScene(new Scene(new StackPane(), 300, 250));
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
         primaryStage.show();
+    }
+    
+    public static void main(String[] args)
+    {
+        launch(args);
     }
 }
