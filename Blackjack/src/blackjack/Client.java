7fcd508e6a5dc8b6784f9a5e324576e2507603b6
Tanner Lisonbee <lisonbeetanner@gmail.com>
2017-11-06 09:41:25 -0700

    Clients can now connect to server over local network

--- Blackjack/src/blackjack/Client.java
+++ Blackjack/src/blackjack/Client.java
@@ -1,17 +1,25 @@
 package blackjack;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.text.NumberFormat;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
  * @author Tanner Lisonbee
  */
-public class Client extends Application
+public class Client extends Application implements Runnable
 {
+    private DataInputStream fromServer;
+    private DataOutputStream toServer;
+    
     @Override
     public void start(Stage primaryStage)
     {
@@ -31,6 +39,39 @@ public class Client extends Application
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
         primaryStage.show();
+        
+        connectToServer();
+    }
+    
+    private void connectToServer()
+    {
+        try
+        {
+            //new socket on port 8000, looking on local network for server
+            Socket socket = new Socket("localhost", 8000);
+            fromServer = new DataInputStream(socket.getInputStream());
+            toServer = new DataOutputStream(socket.getOutputStream());
+        }
+        catch (IOException e)
+        {
+            System.err.println(e);
+        }
+        
+        //new thread for connecting to server
+        new Thread(this).start();
+    }
+    
+    @Override
+    public void run() 
+    {
+        try
+        {
+            //To-do ~ implement data passing on client side
+        }
+        catch (Exception e)
+        {
+            System.err.println(e);
+        }
     }
     
     public static void main(String[] args)
