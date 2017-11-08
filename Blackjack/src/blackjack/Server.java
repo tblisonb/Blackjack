c042c7737026105aa693752f9d07e46ab6cb0bfa
Tanner Lisonbee <lisonbeetanner@gmail.com>
2017-11-08 15:41:29 -0700

    Combined HandleSession and Server into one java file

--- Blackjack/src/blackjack/Server.java
+++ Blackjack/src/blackjack/Server.java
@@ -5,8 +5,10 @@
  */
 package blackjack;
 
+import static blackjack.BlackjackConstants.MAX_PLAYER_COUNT;
 import java.io.BufferedReader;
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
@@ -114,5 +116,53 @@ public class Server extends Application implements BlackjackConstants {
 
     public static void main(String[] args) {
         launch(args);
+    }
+}
+
+/**
+ *
+ * @author Tanner Lisonbee
+ */
+class HandleSession implements Runnable, BlackjackConstants
+{
+    private Player[] players;
+    private final int sessionNum;
+    private TextArea log;
+    
+    private DataInputStream[] fromClient;
+    private DataOutputStream[] toClient;
+    
+    /**
+     * @param socket client socket
+     * @param sessionNum numerical id for client
+     * @param log reference to the server log
+     */
+    public HandleSession(Player[] players, int sessionNum, TextArea log)
+    {
+        this.players = players;
+        this.sessionNum = sessionNum;
+        this.log = log;
+    }
+    
+    @Override
+    public void run() 
+    {
+        try
+        {
+            for (int i = 0; i < MAX_PLAYER_COUNT; i++)
+            {
+                fromClient[i] = new DataInputStream(players[i].getSocket().getInputStream());
+                toClient[i] = new DataOutputStream(players[i].getSocket().getOutputStream());
+            }
+            
+            while (true) //runs indefinitely
+            {
+                //To-do ~ Implement data passing on server-side
+            }
+        }
+        catch (Exception e)
+        {
+            System.err.println(e);
+        }
     }
 }
