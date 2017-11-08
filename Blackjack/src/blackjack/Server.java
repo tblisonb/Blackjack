536ed03bafc01e5e7eef1d53ee4d0be33d240c57
HackingXD <dhowarth161@gmail.com>
2017-11-08 15:37:53 -0700

    Added the hit function, a deck in the server class, and a getValue function that returns the value of a hand

--- Blackjack/src/blackjack/Server.java
+++ Blackjack/src/blackjack/Server.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Date;
 import javafx.application.Application;
 import javafx.application.Platform;
@@ -24,18 +25,18 @@ import javafx.stage.WindowEvent;
  *
  * @author Tanner Lisonbee
  */
-public class Server extends Application implements BlackjackConstants
-{
+public class Server extends Application implements BlackjackConstants {
+
     private Player[] players = new Player[MAX_PLAYER_COUNT];
-    
+    Deck deck = new Deck();
+
     @Override
-    public void start(Stage primaryStage) 
-    {
+    public void start(Stage primaryStage) {
         StackPane root = new StackPane();
         TextArea log = new TextArea(); //server output
         log.setEditable(false); //output log shouldn't be editable
         root.getChildren().add(log);
-        
+
         Scene scene = new Scene(root, 600, 400);
         //ensure server terminates when the window is closed
         primaryStage.setOnCloseRequest((WindowEvent event) -> {
@@ -46,64 +47,72 @@ public class Server extends Application implements BlackjackConstants
         primaryStage.setResizable(false);
         primaryStage.setScene(scene);
         primaryStage.show();
-        
-        try
-        {
+
+        try {
             //new socket on port 8000
             ServerSocket serverSocket = new ServerSocket(8000);
             log.appendText(new Date() + ": Server started at socket 8000\n");
-            
+
             //new thread for polling for client sockets.
-            new Thread(() ->
-            {
+            new Thread(()
+                    -> {
                 int sessionNum = 1; //keeps track of what client is connected
                 while (true) //runs indefinitely
                 {
-                    try
-                    {
+                    try {
                         //poll for new client connection
-                        for (int i = 0; i < MAX_PLAYER_COUNT; i++)
-                        {
-                            log.appendText(new Date() + ": Waiting for player " +
-                                (i + 1) + " to join session #" + sessionNum + 
-                                "\n");
-                            
+                        for (int i = 0; i < MAX_PLAYER_COUNT; i++) {
+                            log.appendText(new Date() + ": Waiting for player "
+                                    + (i + 1) + " to join session #" + sessionNum
+                                    + "\n");
+
                             Socket socket = serverSocket.accept();
                             players[i] = new Player(socket);
-                            try
-                            {
+                            try {
                                 DataInputStream reader = new DataInputStream(players[i].getSocket().getInputStream());
                                 String name = reader.readUTF();
-                                if (name == null || name.isEmpty())
+                                if (name == null || name.isEmpty()) {
                                     name = "Anonymous_" + i;
-                                log.appendText(new Date() + ": Player '" + name + 
-                                    "' has joined session #" + sessionNum + "\n");
-                            }
-                            catch (IOException e)
-                            {
+                                }
+                                log.appendText(new Date() + ": Player '" + name
+                                        + "' has joined session #" + sessionNum + "\n");
+                            } catch (IOException e) {
                                 System.err.println(e);
                             }
                         }
-                        log.appendText(new Date() + ": Starting a thread for " + 
-                                "session #" + sessionNum + "\n");
+                        log.appendText(new Date() + ": Starting a thread for "
+                                + "session #" + sessionNum + "\n");
 
                         HandleSession task = new HandleSession(players, sessionNum++, log);
-                    }
-                    catch (IOException e)
-                    {
+                    } catch (IOException e) {
                         System.err.println(e);
                     }
                 }
             }).start();
-        }
-        catch(IOException e)
-        {
+        } catch (IOException e) {
             System.err.println(e);
         }
     }
-    
-    public static void main(String[] args)
-    {
+
+    public void hit(int playerid) {
+        players[playerid].addCardFirstHand(deck.draw());
+        if(getValue(players[playerid].getFirstHand()) > 21){
+        
+        }
+            
+
+    }
+
+    public int getValue(ArrayList<Card> hand) {
+        int sum = 0;
+        for (int i = 0; i < hand.size(); i++) {
+            sum += hand.remove(i).getNumber().value;
+
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
         launch(args);
     }
 }
